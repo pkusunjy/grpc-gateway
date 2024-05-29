b7905bd0618f59a788162200628760f3b9f677b7
sunjiayu <767978963@qq.com>
2024-05-29 09:18:21 +0800

    add one flag about whether enable ssl between gateway and grpc

--- main.go
+++ main.go
@@ -22,7 +22,8 @@ var (
 	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:8123", "gRPC server endpoint")
 	certChain          = flag.String("cert-chain", "/home/work/cert/cert_chain.pem", "cert chain file")
 	privKey            = flag.String("privkey", "/home/work/cert/privkey.key", "privkey")
-	offlineMode        = flag.Bool("is_offline", false, "whether enable ssl certification")
+	offlineModeLocal   = flag.Bool("is_offline_local", false, "whether enable ssl certification on gateway side")
+	offlineModeGrpc    = flag.Bool("is_offline_grpc", false, "whether enable ssl certification between gateway and grpc")
 )
 
 func run() error {
@@ -34,7 +35,7 @@ func run() error {
 	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
 	var opts []grpc.DialOption
-	if *offlineMode {
+	if *offlineModeGrpc {
 		opts = []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
 	} else {
 		creds, err := credentials.NewClientTLSFromFile(*certChain, "")
@@ -65,7 +66,7 @@ func run() error {
 	}
 
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
-	if *offlineMode {
+	if *offlineModeLocal {
 		return http.ListenAndServe(":8124", mux)
 	} else {
 		return http.ListenAndServeTLS(":8124", *certChain, *privKey, mux)
