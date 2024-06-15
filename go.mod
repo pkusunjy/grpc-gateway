1d47fac3e85e0763165fe5fcf5e83cb8469a2dc7
sunjiayu <767978963@qq.com>
2024-06-15 22:45:24 +0800

    implement jsapi

--- go.mod
+++ go.mod
@@ -4,17 +4,19 @@ go 1.22.2
 
 require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0
-	github.com/pkusunjy/openai-server-proto v1.1.5
+	github.com/pkusunjy/openai-server-proto v1.1.6
+	github.com/wechatpay-apiv3/wechatpay-go v0.2.18
 	google.golang.org/grpc v1.64.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/wechatpay-apiv3/wechatpay-go v0.2.18 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240521202816-d264139d666e // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240521202816-d264139d666e // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/wechatpay-apiv3/wechatpay-go/core"
+	"github.com/wechatpay-apiv3/wechatpay-go/core/option"
+	"github.com/wechatpay-apiv3/wechatpay-go/services/payments/jsapi"
+	"github.com/wechatpay-apiv3/wechatpay-go/utils"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
@@ -26,28 +32,84 @@ var (
 	grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:8123", "gRPC server endpoint")
 	certChain          = flag.String("cert-chain", "/home/work/cert/cert_chain.pem", "cert chain file")
 	privKey            = flag.String("privkey", "/home/work/cert/privkey.key", "privkey")
+	apiClientKeyPath   = flag.String("api_client_key_path", "/home/work/certs/apiclient_key.pem", "api_client_key_path")
 	offlineModeLocal   = flag.Bool("is_offline_local", false, "whether enable ssl certification on gateway side")
 	offlineModeGrpc    = flag.Bool("is_offline_grpc", false, "whether enable ssl certification between gateway and grpc")
 )
 
-type AuthConf struct {
-	WxAppID  string `yaml:"wx_appid"`
-	WxMchID  string `yaml:"wx_mchid"`
-	WxSecret string `yaml:"wx_secret"`
+type WxPaymentServiceImpl struct {
+	WxAppID       string `yaml:"wx_appid"`
+	WxMchID       string `yaml:"wx_mchid"`
+	WxMchAPIv3Key string `yaml:"wx_mch_apiv3"`
+	WxSecret      string `yaml:"wx_secret"`
+	WxSerialNo    string `yaml:"wx_serial_no"`
+	WxClient      *core.Client
+	wx_payment.UnimplementedWxPaymentServiceServer
 }
 
-func loadYaml() AuthConf {
+func WxPaymentServiceInitialize(ctx *context.Context) (*WxPaymentServiceImpl, error) {
+	// load keys
 	content, err := os.ReadFile("./conf/auth.yaml")
 	if err != nil {
 		grpclog.Fatal(err)
+		return nil, err
 	}
 	fmt.Println(string(content))
-	authConf := AuthConf{}
-	err = yaml.Unmarshal(content, &authConf)
+	server := WxPaymentServiceImpl{}
+	err = yaml.Unmarshal(content, &server)
 	if err != nil {
 		grpclog.Fatal(err)
+		return nil, err
 	}
-	return authConf
+	// init wx client
+	mchPrivateKey, err := utils.LoadPrivateKeyWithPath(*apiClientKeyPath)
+	if err != nil {
+		grpclog.Fatal("load merchant private key error: ", err)
+		return nil, err
+	}
+	wxClient, err := core.NewClient(
+		*ctx,
+		option.WithWechatPayAutoAuthCipher(server.WxMchID, server.WxSerialNo, mchPrivateKey, server.WxMchAPIv3Key),
+	)
+	if err != nil {
+		grpclog.Fatal("new wechat pay client error: ", err)
+		return nil, err
+	}
+
+	server.WxClient = wxClient
+	return &server, nil
+}
+
+func (server WxPaymentServiceImpl) Jsapi(ctx context.Context, req *wx_payment.JsApiRequest) (*wx_payment.JsApiResponse, error) {
+	svc := jsapi.JsapiApiService{Client: server.WxClient}
+	prepayResp, _, err := svc.PrepayWithRequestPayment(ctx,
+		jsapi.PrepayRequest{
+			Appid:       &server.WxAppID,
+			Mchid:       &server.WxMchID,
+			Description: core.String("MikiAi会员购买"),
+			OutTradeNo:  core.String(req.GetOpenid() + strconv.FormatInt(time.Now().Unix(), 10)),
+			Attach:      core.String("MikiAi会员购买"),
+			NotifyUrl:   core.String("https://mikiai.tuyaedu.com:8124/wx_payment.NotifyService/jsapi_notify_url"),
+			Amount: &jsapi.Amount{
+				Total: core.Int64(1),
+			},
+			Payer: &jsapi.Payer{
+				Openid: core.String(req.GetOpenid()),
+			},
+		},
+	)
+	if err != nil {
+		grpclog.Fatal("call PrepayWithRequestPayment failed error:", err)
+		return nil, err
+	}
+	resp := wx_payment.JsApiResponse{
+		Timestamp: *prepayResp.TimeStamp,
+		NonceStr:  *prepayResp.NonceStr,
+		Package:   "prepay_id=" + *prepayResp.PrepayId,
+		SignType:  *prepayResp.SignType,
+		PaySign:   *prepayResp.PaySign,
+	}
+	return &resp, nil
 }
 
 func run() error {
@@ -89,7 +151,18 @@ func run() error {
 		return err
 	}
 
-	err = wx_payment.RegisterWxPaymentServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+	err = wx_payment.RegisterNotifyServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	// custom routes
+	wxPaymentServer, err := WxPaymentServiceInitialize(&ctx)
+	if err != nil {
+		grpclog.Fatal("WxPaymentServiceInitialize failed error:", err)
+		return err
+	}
+	err = wx_payment.RegisterWxPaymentServiceHandlerServer(ctx, mux, wxPaymentServer)
 	if err != nil {
 		return err
 	}
@@ -104,9 +177,6 @@ func run() error {
 
 func main() {
 	flag.Parse()
-
-	authconf := loadYaml()
-	fmt.Println(authconf)
 
 	if err := run(); err != nil {
 		grpclog.Fatal(err)
