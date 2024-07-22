4f36f961c42e5633fa4bde25c3e6b001202b3f7b
sunjiayu <767978963@qq.com>
2024-07-22 10:01:06 +0800

    add log & make copy of body

--- service/platform/forward.go
+++ service/platform/forward.go
@@ -1,6 +1,7 @@
 package platform
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"io"
@@ -35,8 +36,6 @@ func ForwardServiceInitialize(ctx *context.Context) (*ForwardService, error) {
 }
 
 func (s ForwardService) Forward(ctx *context.Context, w http.ResponseWriter, r *http.Request) {
-	grpclog.Infof("Forward recv raw request:%+v", r)
-
 	parsedURL, err := url.Parse(r.URL.String())
 	if err != nil {
 		errmsg := fmt.Sprintf("Forward failed to parse url err:%v", err)
@@ -49,8 +48,15 @@ func (s ForwardService) Forward(ctx *context.Context, w http.ResponseWriter, r *
 		forwardURL = fmt.Sprintf("%s?%s", forwardURL, parsedURL.RawQuery)
 	}
 	grpclog.Infof("Forward generate url:%v", forwardURL)
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		errmsg := fmt.Sprintf("Forward failed to parse body err:%v", err)
+		grpclog.Error(errmsg)
+		http.Error(w, errmsg, http.StatusInternalServerError)
+	}
+	grpclog.Infof("Forward recv request:%v", string(bodyBytes))
 
-	forwardRequest, _ := http.NewRequestWithContext(*ctx, r.Method, forwardURL, r.Body)
+	forwardRequest, _ := http.NewRequestWithContext(*ctx, r.Method, forwardURL, bytes.NewReader(bodyBytes))
 	forwardResponse, err := http.DefaultClient.Do(forwardRequest)
 	if err != nil {
 		errmsg := fmt.Sprintf("Forward failed to request backend err:%+v", err)
