a61231b1e630b38c66c23746205374bb1b2441ce
sunjiayu <767978963@qq.com>
2024-07-23 19:12:53 +0800

    add code2session

--- service/auth/auth.go
+++ service/auth/auth.go
@@ -2,6 +2,9 @@ package auth
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/pkusunjy/openai-server-proto/auth"
@@ -59,6 +62,30 @@ func (server AuthServiceImpl) GetOssToken(ctx context.Context, req *auth.AuthReq
 		OssEndpoint:        server.AliyunOssEndpoint,
 		OssAccessKeyId:     server.AliyunOssAccessKeyID,
 		OssAccessKeySecret: server.AliyunOssAccessKeySecret,
+	}
+	return &resp, nil
+}
+
+func (server AuthServiceImpl) Jscode2Session(ctx context.Context, req *auth.AuthRequest) (*auth.AuthResponse, error) {
+	url := fmt.Sprintf(code2SessionUrl, server.WxAppID, server.WxSecret, req.Code)
+	grpclog.Infof("code2session gen url: %s", url)
+	code2SessionResp, err := http.DefaultClient.Get(url)
+	if err != nil {
+		grpclog.Errorf("code2session get fail, err:%v", err)
+		return nil, err
+	}
+	defer code2SessionResp.Body.Close()
+	var wxMap map[string]string
+	err = json.NewDecoder(code2SessionResp.Body).Decode(&wxMap)
+	if err != nil {
+		grpclog.Errorf("code2session json decode fail, err:%v", err)
+		return nil, err
+	}
+	grpclog.Infof("code2session wxMap:%+v", wxMap)
+	resp := auth.AuthResponse{
+		Openid:     wxMap["openid"],
+		SessionKey: wxMap["session_key"],
+		Unionid:    wxMap["unionid"],
 	}
 	return &resp, nil
 }
--- service/auth/consts.go
+++ service/auth/consts.go
@@ -1,6 +1,7 @@
 package auth
 
 const (
-	aliyunOssFile = "./conf/aliyun_oss.yaml"
-	wxPaymentFile = "./conf/wx_payment.yaml"
+	aliyunOssFile   = "./conf/aliyun_oss.yaml"
+	wxPaymentFile   = "./conf/wx_payment.yaml"
+	code2SessionUrl = "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code"
 )
