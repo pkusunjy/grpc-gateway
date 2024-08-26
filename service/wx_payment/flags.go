4cb25980c270c8a623f16fbc3a0a19c49de375b4
sunjiayu <767978963@qq.com>
2024-08-27 07:55:01 +0800

    remove unused gflags & conf

--- service/wx_payment/flags.go
+++ service/wx_payment/flags.go
@@ -3,9 +3,8 @@ package wx_payment
 import "flag"
 
 var (
-	apiClientKeyPath  = flag.String("api_client_key_path", "/home/work/cert/apiclient_key.pem", "api_client_key_path")
-	authFile          = flag.String("auth_file", "./conf/wx_payment.yaml", "auth_file")
-	dataPlatformFile  = flag.String("data_platform_file", "./conf/data_platform.yaml", "data_platform_file")
-	whitelistUserFile = flag.String("whitelist_user_file", "./conf/whitelist_user.yaml", "whitelist_user_file")
-	notifyUrl         = flag.String("notify_url", "https://mikiai.tuyaedu.com:8124/wx_payment_notify/jsapi_notify_url", "notify_url")
+	apiClientKeyPath = flag.String("api_client_key_path", "/home/work/cert/apiclient_key.pem", "api_client_key_path")
+	authFile         = flag.String("auth_file", "./conf/wx_payment.yaml", "auth_file")
+	dataPlatformFile = flag.String("data_platform_file", "./conf/data_platform.yaml", "data_platform_file")
+	notifyUrl        = flag.String("notify_url", "https://mikiai.tuyaedu.com:8124/wx_payment_notify/jsapi_notify_url", "notify_url")
 )
